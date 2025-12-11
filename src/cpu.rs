15a55708a07a427eed8f619727e09f786a2e0543
Strokkur24 <strokkur.24@gmail.com>
2025-12-11 14:05:00 +0100

    Record cycles during instruction execution

--- src/cpu.rs
+++ src/cpu.rs
@@ -16,6 +16,8 @@ struct Cpu {
     /// 0b000_0001 => negative flag
     /// </pre>
     processor_status: u8,
+
+    cycle: u32,
 }
 
 /// Instruction reference: https://www.nesdev.org/wiki/Instruction_reference
@@ -30,48 +32,82 @@ impl Instruction {
     }
 }
 
+fn increment_if_crossed(base: u32, addr: usize) -> u32 {
+    if addr <= 0xFF { base } else { base + 1 }
+}
+
 impl Cpu {
     pub fn execute_instruction(&mut self, inst: &Instruction) {
         match inst.op_code {
-            0x69 => self.execute_adc(inst.arguments[0]),
-            0x65 => self.execute_adc(self.get_addr_zero(inst.arguments[0])),
-            0x75 => self.execute_adc(self.get_addr_zero_x(inst.arguments[0])),
-            0x6D => self.execute_adc(self.get_addr_absolute(inst.get_absolute_addr())),
-            0x7D => self.execute_adc(self.get_addr_absolute_x(inst.get_absolute_addr())),
-            0x79 => self.execute_adc(self.get_addr_absolute_y(inst.get_absolute_addr())),
-            0x61 => self.execute_adc(self.get_addr_indexed_indirect(inst.arguments[0])),
-            0x71 => self.execute_adc(self.get_addr_indirect_indexed(inst.arguments[0])),
+            0x69 => self.execute_adc(inst.arguments[0], 2),
+            0x65 => self.execute_adc(self.get_addr_zero(inst.arguments[0]), 3),
+            0x75 => self.execute_adc(self.get_addr_zero_x(inst.arguments[0]), 4),
+            0x6D => self.execute_adc(self.get_addr_absolute(inst.get_absolute_addr()), 5),
+            0x7D => self.execute_adc(
+                self.get_addr_absolute_x(inst.get_absolute_addr()),
+                increment_if_crossed(4, inst.get_absolute_addr() as usize),
+            ),
+            0x79 => self.execute_adc(
+                self.get_addr_absolute_y(inst.get_absolute_addr()),
+                increment_if_crossed(4, inst.get_absolute_addr() as usize),
+            ),
+            0x61 => self.execute_adc(self.get_addr_indexed_indirect(inst.arguments[0]), 6),
+            0x71 => {
+                let memory_index: usize = self.get_addr_indirect_indexed_index(inst.arguments[0]);
+                self.execute_adc(
+                    self.memory[memory_index],
+                    increment_if_crossed(5, memory_index),
+                )
+            }
 
-            0x29 => self.execute_and(inst.arguments[0]),
-            0x25 => self.execute_and(self.get_addr_zero(inst.arguments[0])),
-            0x35 => self.execute_and(self.get_addr_zero_x(inst.arguments[0])),
-            0x2D => self.execute_and(self.get_addr_absolute(inst.get_absolute_addr())),
-            0x3D => self.execute_and(self.get_addr_absolute_x(inst.get_absolute_addr())),
-            0x39 => self.execute_and(self.get_addr_absolute_y(inst.get_absolute_addr())),
-            0x21 => self.execute_and(self.get_addr_indexed_indirect(inst.arguments[0])),
-            0x31 => self.execute_and(self.get_addr_indirect_indexed(inst.arguments[0])),
+            0x29 => self.execute_and(inst.arguments[0], 2),
+            0x25 => self.execute_and(self.get_addr_zero(inst.arguments[0]), 3),
+            0x35 => self.execute_and(self.get_addr_zero_x(inst.arguments[0]), 4),
+            0x2D => self.execute_and(self.get_addr_absolute(inst.get_absolute_addr()), 4),
+            0x3D => self.execute_and(
+                self.get_addr_absolute_x(inst.get_absolute_addr()),
+                increment_if_crossed(4, inst.get_absolute_addr() as usize),
+            ),
+            0x39 => self.execute_and(
+                self.get_addr_absolute_y(inst.get_absolute_addr()),
+                increment_if_crossed(4, inst.get_absolute_addr() as usize),
+            ),
+            0x21 => self.execute_and(self.get_addr_indexed_indirect(inst.arguments[0]), 6),
+            0x31 => {
+                let memory_index: usize = self.get_addr_indirect_indexed_index(inst.arguments[0]);
+                self.execute_and(
+                    self.memory[memory_index],
+                    increment_if_crossed(5, memory_index),
+                )
+            }
 
-            0x0A => self.execute_asl(self.accumulator, |cpu, r| -> () { cpu.accumulator += r }),
-            0x06 => self.execute_asl(self.get_addr_zero(inst.arguments[0]), |cpu, r| -> () {
-                cpu.set_addr_zero(inst.arguments[0], r)
-            }),
-            0x16 => self.execute_asl(self.get_addr_zero_x(inst.arguments[0]), |cpu, r| -> () {
-                cpu.set_addr_zero_x(inst.arguments[0], r)
-            }),
+            0x0A => self.execute_asl(self.accumulator, |cpu, r| -> () { cpu.accumulator += r }, 2),
+            0x06 => self.execute_asl(
+                self.get_addr_zero(inst.arguments[0]),
+                |cpu, r| -> () { cpu.set_addr_zero(inst.arguments[0], r) },
+                5,
+            ),
+            0x16 => self.execute_asl(
+                self.get_addr_zero_x(inst.arguments[0]),
+                |cpu, r| -> () { cpu.set_addr_zero_x(inst.arguments[0], r) },
+                6,
+            ),
             0x0E => self.execute_asl(
                 self.get_addr_absolute(inst.get_absolute_addr()),
                 |cpu, r| -> () { cpu.set_addr_absolute(inst.get_absolute_addr(), r) },
+                6,
             ),
             0x1E => self.execute_asl(
                 self.get_addr_absolute(inst.get_absolute_addr()),
                 |cpu, r| -> () { cpu.set_addr_absolute_x(inst.get_absolute_addr(), r) },
+                7,
             ),
 
             _ => panic!("Unknown op code received: {}", inst.op_code),
         };
     }
 
-    fn execute_adc(&mut self, memory: u8) {
+    fn execute_adc(&mut self, memory: u8, cycles: u32) {
         let result: u16 =
             self.accumulator as u16 + memory as u16 + (self.processor_status & 0b1000_0000) as u16;
         self.set_carry(result > 0xFF);
@@ -81,16 +117,18 @@ impl Cpu {
         );
         self.set_negative(result & 0b1000_0000 == 0b1000_0000);
         self.accumulator = (result & 0xFF) as u8;
+        self.cycle += cycles
     }
 
-    fn execute_and(&mut self, memory: u8) {
+    fn execute_and(&mut self, memory: u8, cycles: u32) {
         let result: u8 = self.accumulator & memory;
         self.set_zero(result == 0);
         self.set_negative(result & 0b1000_0000 == 0b1000_0000);
         self.accumulator = result;
+        self.cycle += cycles
     }
 
-    fn execute_asl<R>(&mut self, value: u8, r: R)
+    fn execute_asl<R>(&mut self, value: u8, r: R, cycles: u32)
     where
         R: Fn(&mut Cpu, u8),
     {
@@ -99,6 +137,7 @@ impl Cpu {
         self.set_zero(result == 0);
         self.set_negative(result & 0b1000_0000 == 0b1000_0000);
         r(self, result);
+        self.cycle += cycles
     }
 
     //<editor-fold desc="Addressing">
@@ -140,9 +179,11 @@ impl Cpu {
     }
     /// (Indirect),Y
     fn get_addr_indirect_indexed(&self, arg: u8) -> u8 {
-        self.memory[self.memory[arg as usize] as usize
-            + (self.memory[(arg as usize + 1) & 256] as usize)
-            << 8 + self.index_y as usize]
+        self.memory[self.get_addr_indirect_indexed_index(arg)]
+    }
+    fn get_addr_indirect_indexed_index(&self, arg: u8) -> usize {
+        self.memory[arg as usize] as usize + (self.memory[(arg as usize + 1) & 256] as usize)
+            << 8 + self.index_y as usize
     }
     //</editor-fold>
 
