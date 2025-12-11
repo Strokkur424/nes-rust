751442ac3eeaf2f75902a4da836545023c263815
Strokkur24 <strokkur.24@gmail.com>
2025-12-11 20:32:07 +0100

    Implement NOP, ORA, ROL, ROR, and other instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,7 @@
 struct Cpu {
     memory: [u8; 0xFFFF],
     program_counter: u16,
+    /// Initially starts at 255. Each push decreases this value by one, each pop increases it.
     stack_pointer: u8,
     accumulator: u8,
     index_x: u8,
@@ -17,6 +18,8 @@ struct Cpu {
     processor_status: u8,
 
     cycle: u32,
+
+    change_interrupt_disable_flag: i8,
 }
 
 /// Instruction reference: https://www.nesdev.org/wiki/Instruction_reference
@@ -43,6 +46,11 @@ fn increment_if_crossed(base: u32, addr: usize) -> u32 {
 
 impl Cpu {
     pub fn execute_instruction(&mut self, inst: &Instruction) {
+        if self.change_interrupt_disable_flag != -1 {
+            self.set_flag_interrupt(self.change_interrupt_disable_flag != 0);
+            self.change_interrupt_disable_flag = -1;
+        }
+
         // branching instructions are special, as they modify the program counter directly instead
         // of simply incrementing it by one. They should be handled first
         if BRANCHING_OP_CODES.contains(&inst.op_code) {
@@ -65,27 +73,7 @@ impl Cpu {
                     self.push(bytes[0]);
                     self.push(bytes[1]);
 
-                    let mut out: u8 = 0b11 << 4;
-                    if self.get_flag_negative() {
-                        out |= 1 << 7
-                    }
-                    if self.get_flag_overflow() {
-                        out |= 1 << 6
-                    }
-                    if self.get_flag_decimal() {
-                        out |= 1 << 3
-                    }
-                    if self.get_flag_interrupt() {
-                        out |= 1 << 2
-                    }
-                    if self.get_flag_zero() {
-                        out |= 1 << 1
-                    }
-                    if self.get_flag_carry() {
-                        out |= 1
-                    }
-
-                    self.push(out);
+                    self.push(self.get_processor_status());
                     0xFFFE
                 }
 
@@ -333,6 +321,106 @@ impl Cpu {
                 5,
             ),
 
+            0xEA => self.cycle += 2, // nop
+
+            0x09 => self.execute_ora(inst.arguments[0], 2),
+            0x05 => self.execute_ora(self.get_addr_zero(inst.arguments[0]), 3),
+            0x15 => self.execute_ora(self.get_addr_zero_x(inst.arguments[0]), 4),
+            0x0D => self.execute_ora(self.get_addr_absolute(inst.get_absolute_addr()), 4),
+            0x1D => self.execute_ora(
+                self.get_addr_absolute_x(inst.get_absolute_addr()),
+                increment_if_crossed(4, inst.get_absolute_addr() as usize),
+            ),
+            0x19 => self.execute_ora(
+                self.get_addr_absolute_y(inst.get_absolute_addr()),
+                increment_if_crossed(4, inst.get_absolute_addr() as usize),
+            ),
+            0x01 => self.execute_ora(self.get_addr_indexed_indirect(inst.arguments[0]), 6),
+            0x11 => {
+                let location: usize = self.get_addr_indirect_indexed_index(inst.arguments[0]);
+                self.execute_ora(self.memory[location], increment_if_crossed(5, location));
+            }
+
+            0x48 => {
+                self.push(self.accumulator);
+                self.cycle += 3;
+            }
+            0x08 => {
+                self.push(self.get_processor_status());
+                self.cycle += 3;
+            }
+            0x68 => {
+                self.accumulator = self.pop();
+                self.set_flag_zero_by_val(self.accumulator);
+                self.set_flag_negative_by_val(self.accumulator);
+                self.cycle += 4;
+            }
+
+            0x28 => {
+                let val: u8 = self.pop();
+                self.set_processor_status(val, true);
+            }
+
+            0x2A => self.execute_rol(self.accumulator, |cpu, r| -> () { cpu.accumulator = r }, 2),
+            0x26 => self.execute_rol(
+                self.get_addr_zero(inst.arguments[0]),
+                |cpu, r| -> () { cpu.set_addr_zero(inst.arguments[0], r) },
+                5,
+            ),
+            0x36 => self.execute_rol(
+                self.get_addr_zero_x(inst.arguments[0]),
+                |cpu, r| -> () { cpu.set_addr_zero_x(inst.arguments[0], r) },
+                5,
+            ),
+            0x2E => self.execute_rol(
+                self.get_addr_absolute(inst.get_absolute_addr()),
+                |cpu, r| -> () { cpu.set_addr_absolute(inst.get_absolute_addr(), r) },
+                6,
+            ),
+            0x3E => self.execute_rol(
+                self.get_addr_absolute_x(inst.get_absolute_addr()),
+                |cpu, r| -> () { cpu.set_addr_absolute_x(inst.get_absolute_addr(), r) },
+                6,
+            ),
+
+            0x6A => self.execute_ror(self.accumulator, |cpu, r| -> () { cpu.accumulator = r }, 2),
+            0x66 => self.execute_ror(
+                self.get_addr_zero(inst.arguments[0]),
+                |cpu, r| -> () { cpu.set_addr_zero(inst.arguments[0], r) },
+                5,
+            ),
+            0x76 => self.execute_ror(
+                self.get_addr_zero_x(inst.arguments[0]),
+                |cpu, r| -> () { cpu.set_addr_zero_x(inst.arguments[0], r) },
+                5,
+            ),
+            0x6E => self.execute_ror(
+                self.get_addr_absolute(inst.get_absolute_addr()),
+                |cpu, r| -> () { cpu.set_addr_absolute(inst.get_absolute_addr(), r) },
+                6,
+            ),
+            0x7E => self.execute_ror(
+                self.get_addr_absolute_x(inst.get_absolute_addr()),
+                |cpu, r| -> () { cpu.set_addr_absolute_x(inst.get_absolute_addr(), r) },
+                6,
+            ),
+
+            0x40 => {
+                let flags: u8 = self.pop();
+                self.set_processor_status(flags, false);
+
+                let low: u8 = self.pop();
+                let high: u8 = self.pop();
+                self.program_counter = u16::from_be_bytes([high, low]);
+                self.cycle += 6;
+            }
+            0x60 => {
+                let low: u8 = self.pop();
+                let high: u8 = self.pop();
+                self.program_counter = u16::from_be_bytes([high, low]) + 1;
+                self.cycle += 6;
+            }
+
             _ => panic!("Unknown op code received: {}", inst.op_code),
         };
         self.program_counter += inst.size as u16
@@ -470,14 +558,81 @@ impl Cpu {
         self.cycle += cycles
     }
 
+    fn execute_ora(&mut self, value: u8, cycles: u32) {
+        self.accumulator |= value;
+        self.set_flag_zero_by_val(self.accumulator);
+        self.set_flag_negative_by_val(self.accumulator);
+        self.cycle += cycles;
+    }
+
+    fn execute_rol<R>(&mut self, value: u8, r: R, cycles: u32)
+    where
+        R: Fn(&mut Cpu, u8),
+    {
+        let result: u8 = (value << 1) | self.get_flag_carry() as u8;
+        self.set_flag_carry((value >> 7) & 1 == 1);
+        self.set_flag_zero_by_val(result);
+        self.set_flag_negative_by_val(result);
+        r(self, result);
+        self.cycle += cycles;
+    }
+
+    fn execute_ror<R>(&mut self, value: u8, r: R, cycles: u32)
+    where
+        R: Fn(&mut Cpu, u8),
+    {
+        let result: u8 = (value >> 1) | ((self.get_flag_carry() as u8) << 7);
+        self.set_flag_carry(value & 1 == 1);
+        self.set_flag_zero_by_val(result);
+        self.set_flag_negative_by_val(result);
+        r(self, result);
+        self.cycle += cycles;
+    }
+
     fn push(&mut self, val: u8) {
         self.memory[self.stack_pointer as usize + 0x0100] = val;
-        self.stack_pointer += 1;
+        self.stack_pointer -= 1;
     }
 
     fn pop(&mut self) -> u8 {
-        self.stack_pointer -= 1;
+        self.stack_pointer += 1;
         self.memory[self.stack_pointer as usize + 0x0100]
+    }
+
+    fn get_processor_status(&self) -> u8 {
+        let mut out: u8 = 0b11 << 4;
+        if self.get_flag_negative() {
+            out |= 1 << 7
+        }
+        if self.get_flag_overflow() {
+            out |= 1 << 6
+        }
+        if self.get_flag_decimal() {
+            out |= 1 << 3
+        }
+        if self.get_flag_interrupt() {
+            out |= 1 << 2
+        }
+        if self.get_flag_zero() {
+            out |= 1 << 1
+        }
+        if self.get_flag_carry() {
+            out |= 1
+        }
+        out
+    }
+
+    fn set_processor_status(&mut self, flags: u8, delay: bool) {
+        self.set_flag_carry(flags & 1 == 1);
+        self.set_flag_zero((flags << 1) & 1 == 1);
+        if delay {
+            self.change_interrupt_disable_flag = ((flags << 2) & 1) as i8;
+        } else {
+            self.set_flag_interrupt((flags << 2) & 1 == 1);
+        }
+        self.set_flag_decimal((flags << 3) & 1 == 1);
+        self.set_flag_overflow((flags << 6) & 1 == 1);
+        self.set_flag_negative((flags << 7) & 1 == 1);
     }
 
     //<editor-fold desc="Addressing">
